8ba4759b94c86761255fab1e4e1ba869d987028f
Arran Schlosberg <me@arranschlosberg.com>
2021-05-09 11:23:11 +0100

    Remove minting from ERC721Tradable and implement DefinedToken that allows minting with an owner-defined ID.

---
+++ contracts/DefinedToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ERC721Tradable.sol";
+
+/**
+ * @title DefinedToken
+ * DefinedToken - an OpenSea-tradable contract with owner-defined token IDs that
+ * also allows setting of the baseTokenURI.
+ */
+contract DefinedToken is ERC721Tradable {
+    string private _baseTokenURI;
+
+    constructor(string memory name, string memory symbol, string baseTokenURI, address proxyRegistryAddress)
+        setBaseTokenURI(baseTokenURI);
+        ERC721Tradable(name, symbol, proxyRegistryAddress)
+    {}
+
+    /*
+     * @dev Mints `tokenId` and safely transfers it to `to`.
+     */
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId, "");
+    }
+
+    function baseTokenURI() override public view returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    /*
+     * @dev Sets the value returned by baseTokenURI().
+    */
+    func setBaseTokenURI(string baseTokenURI) public onlyOwner {
+        _baseTokenURI = baseTokenURI;
+    }
+}
--- contracts/ERC721Tradable.sol
+++ contracts/ERC721Tradable.sol
@@ -16,13 +16,12 @@ contract ProxyRegistry {
 
 /**
  * @title ERC721Tradable
- * ERC721Tradable - ERC721 contract that whitelists a trading address, and has minting functionality.
+ * ERC721Tradable - ERC721 contract that whitelists a trading address.
  */
 abstract contract ERC721Tradable is ERC721Enumerable, Ownable {
     using SafeMath for uint256;
 
     address proxyRegistryAddress;
-    uint256 private _currentTokenId = 0;
 
     constructor(
         string memory _name,
@@ -30,31 +29,6 @@ abstract contract ERC721Tradable is ERC721Enumerable, Ownable {
         address _proxyRegistryAddress
     ) ERC721(_name, _symbol) {
         proxyRegistryAddress = _proxyRegistryAddress;
-    }
-
-    /**
-     * @dev Mints a token to an address with a tokenURI.
-     * @param _to address of the future owner of the token
-     */
-    function mintTo(address _to) public onlyOwner {
-        uint256 newTokenId = _getNextTokenId();
-        _mint(_to, newTokenId);
-        _incrementTokenId();
-    }
-
-    /**
-     * @dev calculates the next token ID based on value of _currentTokenId
-     * @return uint256 for the next token ID
-     */
-    function _getNextTokenId() private view returns (uint256) {
-        return _currentTokenId.add(1);
-    }
-
-    /**
-     * @dev increments the value of _currentTokenId
-     */
-    function _incrementTokenId() private {
-        _currentTokenId++;
     }
 
     function baseTokenURI() virtual public pure returns (string memory);
