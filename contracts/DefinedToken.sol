1eb0be64d1676157c3a6af984b9c340d07923eda
Arran Schlosberg <me@arranschlosberg.com>
2021-05-13 20:46:20 +0100

    Make DefinedToken.sol inherit from AccessControl, and add a minter role.

--- contracts/DefinedToken.sol
+++ contracts/DefinedToken.sol
@@ -3,25 +3,31 @@
 pragma solidity ^0.8.0;
 
 import "./ERC721Tradable.sol";
+import "openzeppelin-solidity/contracts/access/AccessControl.sol";
 
 /**
  * @title DefinedToken
  * DefinedToken - an OpenSea-tradable contract with owner-defined token IDs that
- * also allows setting of the baseTokenURI.
+ * also (a) allows setting of the baseTokenURI; and (b) supports a minter role.
  */
-contract DefinedToken is ERC721Tradable {
+contract DefinedToken is AccessControl, ERC721Tradable {
     string private _baseTokenURI;
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(string memory name, string memory symbol, string memory baseTokenURI_, address proxyRegistryAddress)
         ERC721Tradable(name, symbol, proxyRegistryAddress)
     {
         setBaseTokenURI(baseTokenURI_);
+
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
     }
 
     /*
      * @dev Mints `tokenId` and safely transfers it to `to`.
      */
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
+    function safeMint(address to, uint256 tokenId) public onlyRole(MINTER_ROLE) {
         _safeMint(to, tokenId, "");
     }
 
@@ -34,5 +40,12 @@ contract DefinedToken is ERC721Tradable {
     */
     function setBaseTokenURI(string memory baseTokenURI_) public onlyOwner {
         _baseTokenURI = baseTokenURI_;
+    }
+
+     /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControl, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
